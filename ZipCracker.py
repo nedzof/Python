23261384415ac972d75549b13c4ab318e6bb12f5
nedzof <nedzofetahovic@hotmail.com>
2020-01-02 16:06:11 +0100

    FirstCommit

--- ZipCracker.py
+++ ZipCracker.py
@@ -1,12 +1,24 @@
 import zipfile as zi
 import os
 
+
 def main():
     path = os.path.dirname(__file__) + "/evil.zip"
     zip = zi.ZipFile(path)
-    password = str.encode("1234")
-    zip.extractall(pwd=password)
+    file = open('/home/caruk/PycharmProjects/Python/dictionary.txt')
+    for guess in file.readlines():
+        chance = guess.strip('\n')
+        password = str.encode(chance)
+        print("[*] Cracking Password for: " + chance)
+        try:
+            zip.extractall(pwd=password)
+            print("[+] Found Password: " + chance + "\n")
+        except Exception as e:
+            print("[-] Found Bad: " + chance + "\n")
+            print(e)
+            continue
     zip.close()
+
 
 if __name__ == "__main__":
     main()
