7a5033d6f2c42745aa6765ed2e90a56fb00e0fae
nedzof <nedzofetahovic@hotmail.com>
2020-01-23 21:27:24 +0100

    FirstCommit

--- ZipCracker.py
+++ ZipCracker.py
@@ -6,25 +6,41 @@ import optparse
 from pathlib import Path
 
 
-def extract_file(chance, zip):
-    password = str.encode(chance)
-    zip.extractall(pwd=password)
-    print("[+] Found Password: " + chance + "\n")
+def extract_file(password, zip_file):
+    try:
+        zip_file.extractall(pwd=password)
+        print("[+] Found password = " + password)
+        return True
+    except Exception as e:
+        print("[-] Found Bad: " + chance)
+        return False
 
 
-parser = optparse.OptionParser("usage%prog " + "-f <zipfile> -d <dictionary>")
-parser.add_option('-f', dest='zname', type='string', help='Specify Zip File')
-parser.add_option('-d', dest='dname', type='string', help='specify Dictionary File')
-(options, args) = parser.parse_args()
-if (options.zname is None) | (options.dname is None):
-    print(parser.usage)
-    exit(0)
+def get_cmd_arguments():
+    parser = optparse.OptionParser("usage%prog " + "-f <zipfile> -d <dictionary>")
+    parser.add_option('-f', dest='zname', type='string', help='Specify Zip File')
+    parser.add_option('-d', dest='dname', type='string', help='specify Dictionary File')
+    (options, args) = parser.parse_args()
+    if (options.zname is None):
+        parser.error("Please specifiy ZipFile to crack!")
+    if (options.dname is None):
+        options.dname = "dictionary"
+    return options
+
+
+def get_file(zip_name, extension):
+    data_folder = Path(os.path.dirname(__file__))
+    zip = data_folder / ("%s.%s" % (zip_name, extension))
+    return zip
+
+
 start = time.time()
+print(start)
+option = get_cmd_arguments()
+zip_file = get_file(option.zname, 'zip')
+dic_file = get_file(option.dname, 'txt')
 
-data_folder = Path(os.path.dirname(__file__))
-zipFile_to_open = data_folder / ("{%s}.zip" % options.zname)
-zip = zi.ZipFile(zipFile_to_open)
-dictionary = open('/home/caruk/PycharmProjects/PythonScripts/{}.txt'.format(options.dname))
+dictionary = open(dic_file)
 for guess in dictionary.readlines():
     chance = guess.strip('\n')
     print("[*] Cracking Password for: " + chance)
@@ -32,8 +48,6 @@ for guess in dictionary.readlines():
         t = Thread(target=extract_file, args=(chance, zip))
         t.start()
     except Exception as e:
-        print("[-] Found Bad: " + chance + "\n")
-        print(e)
         continue
-zip.close()
+# zip_file.close()
 print(time.strftime("%H:%M:%S", time.gmtime(time.time() - start)))
